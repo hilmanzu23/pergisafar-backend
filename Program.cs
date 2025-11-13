f626cd6b14f0a1ca6259916c7db34ada7d50c804
Your Name <you@example.com>
2025-11-13 12:28:22 +0800

    feat:

--- Program.cs
+++ Program.cs
@@ -118,7 +118,7 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
-app.UseCors("AllowSpecificOrigin");
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
